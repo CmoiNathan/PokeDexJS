aa55b63b871d1fd2bfbf3511cf06719fe10be4df
Nathan <nathan69004@gmail.com>
2024-03-22 16:46:18 +0100

    Pokedex V3

--- index.js
+++ index.js
@@ -13,126 +13,154 @@
 //----------------------------------------|
 
 
+
+// Créer le modal boostrap
+document.getElementById('pokemon-modal').innerHTML = `<div class="modal fade" id="pokemonModal" tabindex="-1" aria-labelledby="pokemonModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="pokemonModalTitle"></h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <img src="" class="modal-img" id="pokemonModalImg" alt="Pokemon Image">
+                                    <p class="modal-text" id="pokemonModalNumber"></p>
+                                    <p class="modal-text" id="pokemonModalDescription"></p>
+                                    <p class="modal-text" id="pokemonModalEvolution"></p>
+                                </div>
+                            </div>  
+                        </div>
+                    </div>`
+                    
 // Sélectionne l'élément HTML avec l'ID "pokemon-card"
 const PokemonList = document.getElementById("pokemon-card");
 
 // Définit une fonction pour créer les cartes Pokémon
-function PokeCard() {
-    // Effectue une requête pour récupérer les données des 151 premiers Pokémon
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=1025")
-        .then((response) => response.json()) // Convertit la réponse en JSON
-        .then((Info) => {
-            // Boucle à travers les résultats des Pokémon
-            Info.results.forEach((pokemon, index) => {
-                const pokemonId = index + 1; // Ajoute 1 à chaque pokémon = ID du pokemon
-                // Effectue une requête pour récupérer les détails du Pokémon
-                fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
-                    .then((response) => response.json()) // Convertit la réponse en JSON
-                    .then((pokemonData) => {
+async function PokeCard() {
+    try {
+        const PokemonResponse = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+        const InfoPokemon = await PokemonResponse.json();
 
-                        // Crée un élément div pour la carte Pokémon
-                        const Pokemoncard = document.createElement("div");
-                        Pokemoncard.classList.add("card"); // Ajoute la classe "card"
-                        Pokemoncard.style.background = getTypeColor(pokemonData.types[0].type.name);
+        for (const [index, pokemon] of InfoPokemon.results.entries()) {
+            const pokemonId = index + 1;
 
-                        // Crée une classe CSS spéciale pour diviser le fond en diagonale pour les Pokémon ayant deux types
-                        if (pokemonData.types.length === 2) {
-                            const type1 = pokemonData.types[0].type.name;
-                            const type2 = pokemonData.types[1].type.name;
-                            Pokemoncard.style.background = `linear-gradient(to bottom right, ${getTypeColor(type1)}, ${getTypeColor(type1)} 50%, ${getTypeColor(type2)} 50%, ${getTypeColor(type2)})`;
-                        } else {
-                            // Pour les Pokémon avec un seul type
-                            Pokemoncard.style.background = getTypeColor(pokemonData.types[0].type.name);
-                        }
+            const PokemonResponseID = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+            const pokemonData = await PokemonResponseID.json();
 
-                        // Crée un élément img pour l'image du Pokémon
-                        const PokeImg = document.createElement("img");
-                        PokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`; // Définit la source de l'image
-                        PokeImg.style.width = "27vh"; // Mets la taille à 27
-                        PokeImg.classList.add("imgPokemon"); // Ajoute la classe "imgPokemon"
+            const Pokemoncard = document.createElement("div");
+            Pokemoncard.classList.add("card");
 
-                        // Crée un élément div pour le corps de la carte
-                        const PokeBody = document.createElement("div");
-                        PokeBody.classList.add("card-body"); // Ajoute la classe "card-body"
+            if (pokemonData.types.length === 2) {
+                const type1 = pokemonData.types[0].type.name;
+                const type2 = pokemonData.types[1].type.name;
+                Pokemoncard.style.background = `linear-gradient(to bottom right, ${getTypeColor(type1)}, ${getTypeColor(type1)} 50%, ${getTypeColor(type2)} 50%, ${getTypeColor(type2)})`;
+            } else {
+                Pokemoncard.style.background = getTypeColor(pokemonData.types[0].type.name);
+            }
 
-                        // Crée un paragraphe pour afficher le numéro du Pokémon
-                        const PokeNumber = document.createElement("p");
-                        PokeNumber.classList.add("card-text"); // Ajoute la classe "card-text"
-                        PokeNumber.innerHTML = `n° : ${String(pokemonId).padStart(3, '0')}`;
+            const PokeImg = document.createElement("img");
+            PokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
+            PokeImg.style.width = "27vh";
+            PokeImg.classList.add("imgPokemon");
 
-                        // Crée un titre pour afficher le nom du Pokémon
-                        const PokeName = document.createElement("h5");
-                        PokeName.classList.add("card-title"); // Ajoute la classe "card-title"
-                        PokeName.style.color = "white"; // Mets le nom du pokemon en blanc
-                        PokeName.innerHTML = pokemonData.name; // Définit le contenu du titre
+            const PokeBody = document.createElement("div");
+            PokeBody.classList.add("card-body");
 
-                        // Crée un paragraphe pour afficher les types du Pokémon
-                        const PokeTypes = document.createElement("p");
-                        PokeTypes.classList.add("card-text"); // Ajoute la classe "card-text"
-                        PokeTypes.style.backgroundColor = "black"; // Ajoute un fond noir
-                        PokeTypes.style.borderRadius = "10px"; // Arrondi les bords
+            const PokeNumber = document.createElement("p");
+            PokeNumber.classList.add("card-text");
+            PokeNumber.innerHTML = `n° : ${String(pokemonId).padStart(3, '0')}`;
 
-                        // Ajouter l'icône à la couleur appropriée pour chaque type du Pokémon
-                        pokemonData.types.forEach(typePoke => {
-                            const type = typePoke.type.name;
-                            const typeIcon = document.createElement("i");
-                            typeIcon.classList.add("fa-solid", "fa-hurricane");
-                            typeIcon.style.color = getTypeColor(type);
-                            PokeTypes.appendChild(typeIcon);
-                            PokeTypes.appendChild(document.createTextNode(" " + type + " "));
-                        });
+            const PokeName = document.createElement("h5");
+            PokeName.classList.add("card-title");
+            PokeName.style.color = "white";
+            PokeName.innerHTML = pokemonData.name;
 
-                        // Ajoute les éléments à la carte Pokémon
-                        PokeBody.appendChild(PokeName);
-                        PokeBody.appendChild(PokeNumber);
-                        PokeBody.appendChild(PokeTypes);
-                        Pokemoncard.appendChild(PokeImg);
-                        Pokemoncard.appendChild(PokeBody);
+            const PokeTypes = document.createElement("p");
+            PokeTypes.classList.add("card-text");
+            PokeTypes.style.backgroundColor = "black";
+            PokeTypes.style.borderRadius = "10px";
 
-                        // Ajoute un gestionnaire d'événement pour ouvrir la modal au clic sur la carte Pokémon
-                        Pokemoncard.addEventListener("click", () => {
-                            // Ouvre la modal Bootstrap
-                            const pokemonModal = new bootstrap.Modal(document.getElementById('pokemonModal'));
-                            // Met à jour le contenu de la modal avec les informations du Pokémon
-                            document.getElementById('pokemonModalTitle').innerHTML = pokemonData.name;
-                            document.getElementById('pokemonModalImg').src = `https://raw.githubusercontent`;
-                            document.getElementById('pokemonModalTitle').innerHTML = pokemonData.name;
-                            document.getElementById('pokemonModalImg').src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
-                            document.getElementById('pokemonModalNumber').innerHTML = `n° : ${String(pokemonId).padStart(3, '0')}`;
-                            // Récupérer la description du Pokémon
-                            fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`)
-                            .then(response => response.json())
-                            .then(PokeDescription => {
-                                // Filtrer les caractères de contrôle de la description
-                                const description = PokeDescription.flavor_text_entries.find(entry => entry.language.name === "en").flavor_text.replace(/[\n\t\f]/g, ' ');
-                                document.getElementById('pokemonModalDescription').textContent = `Description : ${description}`;
-                            })
-                            const PokemonCry = getPokemonCryUrl(pokemonId);
-                            const sound = new Audio(PokemonCry);
-                            sound.volume = 0.021;
-                            sound.play();
-                            // Affiche la modal
-                            pokemonModal.show();
-                        });
-                        
-                        // Ajoute la carte au conteneur principal PokemonList
-                        PokemonList.appendChild(Pokemoncard);
-                    })
-                    .catch((error) => {
-                        // Gère les erreurs lors de la récupération des détails du Pokémon
-                        console.error("Erreur lors de la récupération des détails du Pokémon:", error);
-                    });
+            pokemonData.types.forEach(typePoke => {
+                const type = typePoke.type.name;
+                const typeIcon = document.createElement("i"); 
+                typeIcon.classList.add("fa-solid", "fa-hurricane");
+                typeIcon.style.color = getTypeColor(type);
+                PokeTypes.appendChild(typeIcon);
+                PokeTypes.appendChild(document.createTextNode(" " + type + " "));
             });
-        })
-        .catch((error) => {
-            // Gère les erreurs lors de la récupération des détails
-            console.error("Erreur lors de la récupération des détails:", error);
-        });
+
+            PokeBody.appendChild(PokeName);
+            PokeBody.appendChild(PokeNumber);
+            PokeBody.appendChild(PokeTypes);
+            Pokemoncard.appendChild(PokeImg);
+            Pokemoncard.appendChild(PokeBody);
+
+            Pokemoncard.addEventListener("click", async () => {
+                const pokemonModal = new bootstrap.Modal(document.getElementById('pokemonModal'));
+                const ChainPokeEvolution = await PokemonEvolutionsChain(pokemonId);
+                const PokeEvolution = ChainPokeEvolution.map(evolution => `<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${evolution.id}.png" alt="${evolution.name}" > `).join(" ");
+                document.getElementById('pokemonModalTitle').innerHTML = pokemonData.name;
+                document.getElementById('pokemonModalImg').src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
+                document.getElementById('pokemonModalNumber').innerHTML = `n° : ${String(pokemonId).padStart(3, '0')}`;
+                document.getElementById('pokemonModalEvolution').innerHTML = `Evolutions : ${PokeEvolution}`;
+                
+                try {
+                    const PokeDescriptionResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`);
+                    const PokeDescription = await PokeDescriptionResponse.json();
+                    const description = PokeDescription.flavor_text_entries
+                        .filter(PokeFilter => PokeFilter.language.name === "en")
+                        .map(descriptionPokemon => descriptionPokemon.flavor_text.replace(/[\n\t\f]/g, ' '));
+                    document.getElementById('pokemonModalDescription').innerHTML = `Description : ${description[0]}`;
+                } catch (error) {
+                    console.error("Erreur lors de la récupération de la description du Pokémon :", error);
+                }
+
+                const PokemonCry = getPokemonCryUrl(pokemonId);
+                const sound = new Audio(PokemonCry);
+                sound.volume = 0.021;
+                sound.play();
+                pokemonModal.show();
+            });
+
+            PokemonList.appendChild(Pokemoncard);
+        }
+    } catch (error) {
+        console.error("Une erreur s'est produite :", error);
+    }
 }
 
 // Appelle la fonction PokeCard pour afficher les cartes Pokémon
 PokeCard();
 
+
+async function PokemonEvolutionsChain(pokemonId) {
+    try {
+        const PokeEvolutionResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`);
+        const data = await PokeEvolutionResponse.json();
+        const evolutionChainURL = data.evolution_chain.url;
+
+        const responseChain = await fetch(evolutionChainURL);
+        const evolutionData = await responseChain.json();
+
+        const chain = [];
+        let current = evolutionData.chain;
+
+        while (current) {
+            if (current.species.name !== data.name) {
+                chain.push({
+                    name: current.species.name,
+                    id: current.species.url.split("/")[6]
+                });
+            }
+            current = current.evolves_to[0];
+        }
+
+        return chain;
+    } catch (error) {
+        console.error("Erreur lors de la récupération de la chaîne d'évolution:", error);
+        return [];
+    }
+}
 
 //----------------------------------------|
 //                                        |
@@ -224,9 +252,12 @@ Scroll();
 //----------------------------------------|
 
 // Définit une fonction pour la recherche de Pokémon
+// Définit une fonction pour la recherche de Pokémon
 function SearchPoke() {
     // Récupère la valeur saisie dans le champ de recherche et la met en minuscules
-    const pokeFound = document.getElementById("pokemon-name").value.toLowerCase();
+    const searchString = document.getElementById("pokemon-name").value.trim().toLowerCase();
+    // Récupère le type sélectionné dans la liste déroulante
+    const selectedType = document.getElementById("pokemon-type").value.toLowerCase();
     // Sélectionne le champ de recherche
     const inputField = document.getElementById("pokemon-name");
 
@@ -237,22 +268,34 @@ function SearchPoke() {
 
     // Parcourt chaque carte Pokémon
     pokemonCards.forEach(card => {
-        // Récupère le nom du Pokémon associé à la carte et le met en minuscule
-        const pokemonName = card.querySelector(".card-title").textContent.toLowerCase();
+        // Récupère les types du Pokémon associé à la carte
+        const pokemonTypes = Array.from(card.querySelectorAll(".card-text")).map(type => type.textContent.trim().toLowerCase());
         
-        // Vérifie si le nom du Pokémon contient le texte saisi dans le champ de recherche
-        if (pokemonName.includes(pokeFound)) {
-            // Affiche la carte si le nom correspond
-            card.style.display = "block";
+        // Vérifie si le type sélectionné est "Tous les types" ou si le type du Pokémon correspond au type sélectionné
+        if (selectedType === 'all' || pokemonTypes.includes(selectedType)) {
+            // Récupère le nom du Pokémon associé à la carte et le met en minuscule
+            const pokemonName = card.querySelector(".card-title").textContent.trim().toLowerCase();
+            // Récupère l'ID du Pokémon associé à la carte
+            const pokemonId = card.querySelector(".card-number").textContent.trim();
+            // Vérifie si le nom ou l'ID du Pokémon correspond à la recherche
+            if (pokemonName.includes(searchString) || pokemonId === searchString) {
+                // Affiche la carte si le nom ou l'ID correspond et que le type correspond
+                card.style.display = "block";
+            } else {
+                // Masque la carte si le nom ou l'ID ne correspond pas mais que le type correspond
+                card.style.display = "none";
+            }
         } else {
-            // Masque la carte si le nom ne correspond pas
+            // Masque la carte si le type du Pokémon ne correspond pas au type sélectionné
             card.style.display = "none";
         }
     });
 }
 
+
 // Appelle la fonction de recherche de Pokémon
 SearchPoke();
+
 
 //----------------------------------------|
 //                                        |
@@ -260,11 +303,18 @@ SearchPoke();
 //                                        |
 //----------------------------------------|
 
-// Sélectionne l'élément audio
+
+// Déclarez la variable BackgroundMusicPoke en dehors de la fonction DOMContentLoaded
 const BackgroundMusicPoke = document.getElementById("background-music");
 
-// Baisse le volume à 0.1
-BackgroundMusicPoke.volume = 0.021;
+// Attendez que le DOM soit entièrement chargé
+document.addEventListener("DOMContentLoaded", function() {
+    // Démarrez la lecture de l'audio ici
+    BackgroundMusicPoke.volume = 0.021;
+    BackgroundMusicPoke.play();
+});
+
+
 
 // Définit une fonction pour réinitialiser et lire la musique en boucle
 function playBackgroundMusic() {
